6583893acac1fc88a541d182e19073b3e8bfd8d4
aliaoc <150737450+aliaoc@users.noreply.github.com>
2026-03-12 17:05:42 +0300

    Delete md directory

--- md/Leng.f90
+++
@@ -1,9 +0,0 @@
-      function leng( str )
-        character str*(*)
-
-        do leng = len(str), 1, -1
-           if ( str(leng:leng).ne.' ' ) return
-        end do
-
-        return
-      end function leng
